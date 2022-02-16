e7e1a0b88c5c549e0e72dc0b44d36f91155f4a59
curonny <rmontano1992@gmail.com>
2022-02-16 14:54:18 -0500

    start session if not open

--- client_access/controllers/controllers.py
+++ client_access/controllers/controllers.py
@@ -12,6 +12,8 @@ import xmlrpc.client
 from odoo import http
 from odoo.http import content_disposition, dispatch_rpc, request
 
+import xmlrpc.client
+
 
 class Controllers(http.Controller):
 
@@ -29,10 +31,43 @@ class Controllers(http.Controller):
     def web_login(self, redirect=None, **kw):
         domain_url = "http://localhost:8023"
         action = 'pos.ui'
-        username = 'admin'
-        password = 'admin'
-        url = domain_url + '/login_odoo?db=' + '9999' + '&login=' + username + '&password=' + password + '&action=' + action
-        return werkzeug.utils.redirect(url)
+        db = kw.get('db_selection')
+        username = kw.get('login')
+        password = kw.get('password')
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(domain_url))
+        print(common)
+        try:
+            uid = common.authenticate('tc9999', username, password, {})
+            pos_config = False
+            models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(domain_url))
+            session = models.execute_kw('tc9999', uid, password, 'pos.session', 'search_read',
+                                        [[['user_id', '=', uid], ['state', '=', 'opened']]])
+
+            # Revisando si el usuario tiene session activa
+            if session:
+                url = domain_url + '/login_odoo?db=' + 'tc9999' + '&login=' + username + '&password=' + password + '&action=' + action
+
+                return werkzeug.utils.redirect(url)
+
+            # SI NO TIENE SESSION ACTIVA, BUSCAR UN POS para INICIALIZAR
+            pos_configs = models.execute_kw('tc9999', uid, password, 'pos.config', 'search_read',
+                                            [[]])
+            for pos in pos_configs:
+                pos_sessions = models.execute_kw('tc9999', uid, password, 'pos.session', 'search_read',
+                                                 [[['config_id', '=', pos.get('id')], ['state', '=', 'opened']]])
+                if not pos_sessions:
+                    pos_config = pos.get('id')
+            # IF NOT POS CONFIG ERROR NO ENTRAR!
+            # ELSE
+
+            start_session_in_config = models.execute_kw('tc9999', uid, password, 'pos.config', 'open_session_cb',
+                                                        [pos_config])
+
+            url = domain_url + '/login_odoo?db=' + 'tc9999' + '&login=' + username + '&password=' + password + '&action=' + action
+
+            return werkzeug.utils.redirect(url)
+        except:
+            pass
 
     @http.route('/redirect_odoo', type='http', auth='public', website=True)
     def page_redirect_odoo(self, **kw):
