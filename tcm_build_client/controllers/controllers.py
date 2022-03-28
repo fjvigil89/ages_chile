8bdfa1d29941bc4704faa082f6af4a48581bcf9e
Jose Ramon Vidal <jramonholy@gmail.com>
2022-03-28 12:00:32 -0400

    Commit Modulos del TCM y Readme 28-03-2022

--- tcm_build_client/controllers/controllers.py
+++ tcm_build_client/controllers/controllers.py
@@ -78,19 +78,35 @@ class TCMPlatformWizard(http.Controller):
             status['message'] = error
             # print(error)
 
+        # Instalando los modulos necesarios
+        self.install_odoo_modules()
+        print("Modulos instalados con exito!")
+
         return request.render("tcm_build_client.build_client_wizard_report", status)
         # Finished!
 
-    # If you want to install module on any button event from your custome module you can use below code:
     def install_odoo_modules(self):
+        # You can use this client if you have Erppeek installed and have a erppeek.ini file
+        # client = erppeek.Client.from_config('ErpPeekDemoDatabase')
+        # The alternative is by specifying the settings by command
+        client = erppeek.Client('http://localhost:8069', '4444', 'chichomentira@tcm.com', 'desarrollo')
+        print('Instalando los modulos...')
+        modules = client.modules('point_of_sale', installed=False)
+        if 'point_of_sale' in modules['uninstalled']:
+            client.install('tcm_client_access','hr','point_of_sale','website_sale')
+            print('Los modulos seleccionados han sido instalado!')
+
+
+    # If you want to install module on any button event from your custome module you can use below code:
+    def install_odoo_modules_test1(self):
         pool = request.env['ir.module']
         module_obj = pool.get('ir.module.module')
-        module_list = ['sale','purchase']
+        module_list = ['web','point_of_sale','tcm_client_access']
         module_ids_to_install = module_obj.search(cr, 1, [('name','in',module_list)])
         module_obj.update_list(cr, 1, context=None)  # just to update module list to update new module added
         module_obj.button_immediate_install(cr, 1, module_ids_to_install, context=context)
 
-    def install_odoo_modules2(self):
+    def install_odoo_modules_test2(self):
         irModuleObj = request.env['ir.module.module']
         irModuleObj.update_list()
         moduleIds = irModuleObj.search(
@@ -102,31 +118,32 @@ class TCMPlatformWizard(http.Controller):
         if moduleIds:
             moduleIds[0].button_immediate_install()
 
-    @http.route('/build_client/database/example', type='http', auth="public", website=True, csrf=False)
+    @http.route('/build_client/database/test_install', type='http', auth="public", website=True, csrf=False)
     def wizard_example(self, **d):
         SERVER = 'http://localhost:8069'
-        ADMIN_PASSWORD = 'badpassword'
-        DATABASE = 'bad9999'
-        LOGIN = 'jramonholy@gmail.com'
+        ADMIN_PASSWORD = 'desarrollo'
+        DATABASE = '4444'
+        LOGIN = 'chichomentira@tcm.com'
         USER_PASSWORD = 'desarrollo'
         LANG = 'es_CL'
         COUNTRY_CODE = 'cl'
         DEMO = False
 
         status = {}
-        try:
-            client = erppeek.Client(server=SERVER)
-            if not DATABASE in client.db.list():
-                print("la base de datos no existe, creando una!")
-                client.create_database(ADMIN_PASSWORD, DATABASE, DEMO, LANG, USER_PASSWORD, LOGIN, COUNTRY_CODE)
-                status['message'] = 'Instancia de Odoo (" % DATABASE % ") se creó con éxito!!!'
-            else:
-                print("La base de datos " % DATABASE % " ya existe.")
-                status['message'] = "La base de datos (" % DATABASE % ") ya existe, por favor seleccione otro nombre."
-        except Exception as e:
-            error = "Database creation error: %s" % (str(e) or repr(e))
-            status['message'] = error
-            print(error)
+
+        client = erppeek.Client(server=SERVER)
+        if not DATABASE in client.db.list():
+            print("La base de datos no existe, creando una!")
+            client.create_database(ADMIN_PASSWORD, DATABASE, DEMO, LANG, USER_PASSWORD, LOGIN, COUNTRY_CODE)
+            status['message'] = 'Instancia de Odoo (" % DATABASE % ") se creó con éxito!!!'
+            print('Usuario: ', client.user)
+        else:
+            print("La base de datos " % DATABASE % " ya existe.")
+            status['message'] = "La base de datos (" % DATABASE % ") ya existe, por favor seleccione otro nombre."
+
+        # Instalando los modulos necesarios
+        self.install_odoo_modules()
+        print("Modulos instalados con exito!")
 
         ip_address = request.httprequest.environ['REMOTE_ADDR']
         print('IP del cliente: ', ip_address)
